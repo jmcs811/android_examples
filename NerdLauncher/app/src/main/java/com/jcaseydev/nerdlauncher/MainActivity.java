e5de1c223773a990506b6ddec29b53a34fff2bf6
jmcs811 <justincaseyy69@gmail.com>
2019-04-08 18:38:34 -0400

    Finished NerdLauncher. Show all apps listed on device along with the associated icon. Can be set to default launcher.

--- NerdLauncher/app/src/main/java/com/jcaseydev/nerdlauncher/MainActivity.java
+++ NerdLauncher/app/src/main/java/com/jcaseydev/nerdlauncher/MainActivity.java
@@ -1,13 +1,13 @@
 package com.jcaseydev.nerdlauncher;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected Fragment createFragment() {
+        return NerdLauncherFragment.newInstance();
     }
 }
