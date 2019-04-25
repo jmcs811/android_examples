707f7f1b32250f412c1087bfb0040b68dc9ad966
jmcs811 <justincaseyy69@gmail.com>
2019-04-25 17:53:50 -0400

    Added map view with markers on the map.

--- Locatr/app/src/main/java/com/jcaseydev/locatr/FlickrFetchr.java
+++ Locatr/app/src/main/java/com/jcaseydev/locatr/FlickrFetchr.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class FlickrFetchr {
 
     private static final String TAG = "FlickerFetchr";
-    private static final String API_KEY = "9df31b6fd41e9a73265991e3b0948700";
+    private static final String API_KEY = "API_KEY_HERE";
     private static final String FETCH_RECENTS_METHOD = "flicker.photos.getRecent";
     private static final String SEARCH_METHOD = "flickr.photos.search";
     private static final Uri ENDPOINT = Uri
