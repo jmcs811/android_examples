93e10eeac4e204eb7509b5976473785d3d248730
jmcs811 <justincaseyy69@gmail.com>
2019-04-26 20:48:29 -0400

    Starting android developer fundamentals course.

---
+++ AndroidDeveloperFundamentals/app/src/main/java/com/jcaseydev/hellotoast/MainActivity.java
@@ -0,0 +1,33 @@
+package com.jcaseydev.hellotoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mShowCount = findViewById(R.id.show_count);
+
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(this, "The count is " + mCount, Toast.LENGTH_SHORT).show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+}
